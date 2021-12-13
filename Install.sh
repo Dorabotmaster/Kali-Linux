00852f9d939850f6f07d3fc64bc2ab681c5d4c28
Kyberspace <89725290+Dorabotmaster@users.noreply.github.com>
2021-12-13 19:13:08 +0530

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,1 +1,31 @@
 
+#colours
+cyan='\033[1;36m'
+green='\033[1;32m'
+red='\033[1;31m'
+yellow='\033[1;33m'
+b='\033[1;34m'
+p='\033[1;35m'
+reset='\033[0m'
+echo " "
+echo " $red
+"WELCOME TO KALI LINUX ANDROID "
+echo " $red
+" NOTE:this version is not official."
+
+cd $HOME
+mkdir KALI-LINUX
+
+cd KALI-LINUX
+mkdir InformationGathering
+mkdir VulnerabilityAnalysis
+mkdir WebApplicationAnalysis
+mkdir DatabaseAssessment
+mkdir PasswordAttacks
+mkdir WirelessAttacks
+mkdir ReverseEngineering
+mkdir ExploitationTools
+mkdir SniffingandSpoofing
+mkdir PostExploitation
+mkdir Forensics
+mkdir ReportingTools
