c557a5d1754cda63f818d05aa8a1e8b61d4a4b44
Kyberspace <89725290+Dorabotmaster@users.noreply.github.com>
2021-12-13 19:14:29 +0530

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -29,3 +29,51 @@ mkdir SniffingandSpoofing
 mkdir PostExploitation
 mkdir Forensics
 mkdir ReportingTools
+mkdir SocialEngineering Tools
+cd $InformationGathering
+apt install nmap
+cd $HOME
+cd Kal
+cd VulnerabilityAnalysis
+apt install sudo
+apt install bed
+sudo apt install ohrwurm
+sudo apt-get install powerfuzzer
+sudo apt install siparmyknife
+sudo apt install sfuzz
+cd $HOME
+cd KALI-LINUX
+cd $WebApplicationAnalysis
+
+sudo apt install burpsuite
+sudo apt-get install httrack
+sudo apt install sqlmap
+sudo apt install webscarab
+sudo apt install wpscan
+
+cd $HOME
+cd KALI-LINUX
+cd DatabaseAssessment
+
+sudo apt-get install bbqsql
+sudo apt install jsql-injection
+sudo apt install jsql-injection
+sudo apt install oscanner
+sudo apt install sqlninja
+sudo apt install tnscmd10g
+cd $HOME
+cd $KALI-LINUX
+cd $PasswordAttacks
+sudo apt install cewl
+sudo apt install crunch
+sudo apt install hashcat
+sudo apt install john
+sudo apt install johnny
+sudo apt-get install medusa
+cd $HOME
+cd $KALI-LINUX
+cd $WirelessAttacks
+sudo apt install aircrack-ng
+sudo apt install fern-wifi-cracker
+sudo apt install kismet
+sudo apt-get install ghost-phisher
